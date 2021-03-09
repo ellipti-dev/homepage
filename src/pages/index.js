fcc2e29aa41d9d1ca96e538870f70d0e430abb9b
gnujoow <gnujoow.kim@gmail.com>
2021-03-10 02:14:03 +0900

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,68 @@
-import * as React from "react"
+import * as React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 
-import Layout from '../components/layout';
-import Jumbotron from '../components/jumbotron';
+import Layout from "../components/layout";
+import Jumbotron from "../components/jumbotron";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-  },
+  root: {},
   jumbotron: {
+    backgroundImage: "url('/svg/index1.svg')",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "right 20% center",
   },
   title: {
-    fontSize: '60px',
-    color: '#fff',
-    fontWeight: 'normal',
+    fontSize: "60px",
+    color: "#fff",
+    fontFamily: '"Roboto Slab", serif',
+    fontWeight: "normal",
     lineHeight: 1.3,
-    textDecoration: 'underline',
-    width: '60%',
+    textDecoration: "underline",
+    width: "60%",
   },
   visionSection: {
     paddingTop: 73,
     paddingBottom: 86,
-    backgroundColor: '#F8FAFB',
+    backgroundColor: "#E4E9EB",
   },
   visionTitle: {
     fontSize: 20,
-    fontWeight: 'normal',
+    fontFamily: '"Roboto Slab", serif',
+    fontWeight: "normal",
   },
   visionDescription: {
     fontSize: 16,
+    fontWeight: 300,
   },
   valueSection: {
     paddingTop: 44,
     paddingBottom: 160,
+
+    '& $valueContainer:nth-child(2n+1)': {
+      flexDirection: 'row-reverse',
+    },
+    '& $valueContainer:nth-child(2n) img': {
+      float: 'right',
+    }
   },
   valueTitle: {
     fontSize: 20,
-    fontWeight: 'normal',
+    fontFamily: '"Roboto Slab", serif',
+    fontWeight: "normal",
   },
   valueDescription: {
-    fontSize: 16,
+    fontSize: 14,
+    fontWeight: 300,
   },
   valueContainer: {
     paddingTop: 81,
-  }
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 }));
 
 // markup
@@ -55,94 +71,96 @@ const IndexPage = () => {
   return (
     <Layout className={classes.root}>
       <Jumbotron className={classes.jumbotron}>
-        <Typography
-          className={classes.title}
-          component={'h1'}
-          gutterBottom
-        >
+        <Typography className={classes.title} component={"h1"} gutterBottom>
           Building with the founders, accelerate the shift
         </Typography>
       </Jumbotron>
-  
+
       <section className={classes.visionSection}>
         <Container>
           <Grid container>
             <Grid item xs={12} sm={2}>
               <Typography
                 className={classes.visionTitle}
-                component='h2'
+                component="h2"
                 gutterBottom
               >
                 Vision
               </Typography>
             </Grid>
-             <Grid item xs={12} sm={10}>
+            <Grid item xs={12} sm={10}>
               <Typography
                 className={classes.visionDescription}
-                component='p'
+                component="p"
                 gutterBottom
               >
-                Ecosystem builder for the blockchain companies to navigate the market pursuing excellence with the right strategy and implementations.
+                Ecosystem builder for the blockchain companies to navigate the
+                market pursuing excellence with the right strategy and
+                implementations.
               </Typography>
             </Grid>
           </Grid>
         </Container>
       </section>
+
       <section className={classes.valueSection}>
         <Container>
           <Typography
             className={classes.valueTitle}
-            component='h2'
-            align={'center'}
+            component="h2"
+            align={"center"}
             gutterBottom
           >
             What is <br />
             Ellipti’s value proposition?
           </Typography>
 
-          <Grid 
-            className={classes.valueContainer}
-            container
-            spacing={8}
-          >
+          <Grid className={classes.valueContainer} container spacing={8}>
             <Grid item xs={12} sm={6}>
-              <Typography
-                align={'left'}
-                gutterBottom
-              >
-                {'From a project to the company with sustainable growth backed by generalists & specialists.'}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <img src='https://dummyimage.com/600x400/000/fff&text=TBD' alt='tbd' />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <img src='https://dummyimage.com/600x400/000/fff&text=TBD' alt='tbd' />
+              <img src="/svg/index2.svg" alt="tbd" />
             </Grid>
             <Grid item xs={12} sm={6}>
               <Typography
-                align={'left'}
+                className={classes.valueDescription}
+                align={"left"}
                 gutterBottom
               >
-                {'Ellipti covers overall necessities in the crypto market with the expertise and various experiences'}
+                {
+                  "From a project to the company with sustainable growth backed by generalists & specialists."
+                }
               </Typography>
             </Grid>
+          </Grid>
+
+          <Grid className={classes.valueContainer} container spacing={8}>
             <Grid item xs={12} sm={6}>
-              <Typography
-                align={'left'}
-                gutterBottom
-              >
-                {'Ellipti advisory & Ellipti labs accelerate teams mainly in the Korean market, further to the other major local markets, aiming to grow the sustainable & strong ecosystem'}
-              </Typography>
+              <img src="/svg/index3.svg" alt="tbd" />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <img src='https://dummyimage.com/600x400/000/fff&text=TBD' alt='tbd' />
+              <Typography className={classes.valueDescription} gutterBottom>
+                {
+                  "Ellipti covers overall necessities in the crypto market with the expertise and various experiences"
+                }
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid className={classes.valueContainer} container spacing={8}>
+            <Grid item xs={12} sm={6}>
+              <img src="/svg/index4.svg" alt="tbd" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography className={classes.valueDescription} gutterBottom>
+                {
+                  "Ellipti advisory & Ellipti labs accelerate teams mainly in the Korean market, further to the other major local markets, aiming to grow the sustainable & strong ecosystem"
+                }
+              </Typography>
             </Grid>
           </Grid>
         </Container>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
