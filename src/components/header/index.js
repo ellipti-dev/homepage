cdfc57f812efddff5cc41f5af46c178e0699a8a3
gnujoow <gnujoow.kim@gmail.com>
2021-03-14 04:16:08 +0900

    fix header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,37 +1,49 @@
-import React, { useState } from 'react';
-import { Link } from 'gatsby';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    background: '#000',
+    background: "#000",
   },
   grow: {
     flexGrow: 1,
   },
+  expandButton: {
+    color: "rgb(255, 255, 255, 0.7)",
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
-  menuItems: {
-    color: '#fff',
+  menuItem: {
+    margin: "0 14px",
+    padding: "2px 0",
+
+    color: "rgb(255, 255, 255, 0.7)",
+    textDecoration: "none",
   },
+  menuItemActive: {
+    borderBottom: "2px solid white",
+    color: "#fff",
+  },
+  mobileMenuItem: {},
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
   },
   sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
   },
 }));
@@ -49,25 +61,44 @@ export default function Header() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      <MenuItem className={classes.menuItems} component={Link} to={'/about'}>
+      <MenuItem
+        className={classes.mobileMenuItem}
+        component={Link}
+        to={"/about"}
+      >
         About
       </MenuItem>
-      <MenuItem className={classes.menuItems} component={Link} to={'/portfolio'}>
+      <MenuItem
+        className={classes.mobileMenuItem}
+        component={Link}
+        to={"/portfolio"}
+      >
         Portfolio
       </MenuItem>
-      <MenuItem className={classes.menuItems} component={Link} to={'/labs'}>
+      <MenuItem
+        className={classes.mobileMenuItem}
+        component={Link}
+        to={"/labs"}
+      >
         Labs
+      </MenuItem>
+      <MenuItem
+        className={classes.mobileMenuItem}
+        component={Link}
+        to={"/public"}
+      >
+        Public
       </MenuItem>
     </Menu>
   );
@@ -83,34 +114,53 @@ export default function Header() {
         <Toolbar>
           <Button
             component={Link}
-            to={'/'}
+            to={"/"}
             className={classes.menuButton}
             color="inherit"
             aria-label="open drawer"
           >
-            <img src='/logo/ellipti.svg' alt='ellipti logo' />
+            <img src="/logo/ellipti.svg" alt="ellipti logo" />
           </Button>
 
           <div className={classes.grow} />
-        
+
           <div className={classes.sectionDesktop}>
-            <Button component={Link} className={classes.menuItems} to={'/about'} color="inherit">
+            <Link
+              to={"/about"}
+              className={classes.menuItem}
+              activeClassName={classes.menuItemActive}
+            >
               About
-            </Button>
-            <Button component={Link} className={classes.menuItems} to={'/portfolio'} color="inherit">
+            </Link>
+            <Link
+              to={"/portfolio"}
+              className={classes.menuItem}
+              activeClassName={classes.menuItemActive}
+            >
               Portfolio
-            </Button>
-            <Button component={Link} className={classes.menuItems} to={'/labs'} color="inherit">
+            </Link>
+            <Link
+              to={"/labs"}
+              className={classes.menuItem}
+              activeClassName={classes.menuItemActive}
+            >
               Labs
-            </Button>
+            </Link>
+            <Link
+              to={"/public"}
+              className={classes.menuItem}
+              activeClassName={classes.menuItemActive}
+            >
+              Public
+            </Link>
           </div>
           <div className={classes.sectionMobile}>
             <IconButton
+              className={classes.expandButton}
               aria-label="show more"
               aria-controls={mobileMenuId}
               aria-haspopup="true"
               onClick={handleMobileMenuOpen}
-              color="inherit"
             >
               <MoreIcon />
             </IconButton>
